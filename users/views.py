2254316ebb521a34f764d5ccaf83a7b30aca97ea
choding91 <whdlsrjf91@naver.com>
2023-02-01 21:02:27 +0900

    Feat : 유저 로그아웃 기능
    - 쿠키 삭제 및 토큰 유효 기간 최소화 완료
    Close #4

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from users.serializers import UserSerializer
 
 
-# 회원가입
+# 회원가입 / 로그아웃
 class SignupView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -13,3 +13,12 @@ class SignupView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):  # JS localStorage.clear() or localStorage.removeItem("") 처리 필요
+        if request.user.is_authenticated:
+            response = Response({"message": "로그아웃이 완료되었습니다."}, status=status.HTTP_204_NO_CONTENT)
+            response.delete_cookie("access")
+            response.delete_cookie("refresh")
+            return response
+        else:
+            return Response({"message": "로그인 정보를 확인해 주세요."}, status=status.HTTP_400_BAD_REQUEST)
