54d9620776d54c0209114d3f27fa055aed5c2a57
choding91 <whdlsrjf91@naver.com>
2023-02-01 21:26:43 +0900

    Test : 유저 회원가입 테스트
    - 유저 회원가입 테스트 케이스 작성 및 확인 완
    Close #5

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from users.models import User
 
-# Create your tests here.
+
+class UserTest(APITestCase):
+    def test_signup(self):
+        response = self.client.post(
+            path=reverse("signup_view"),
+            data={"email": "email@email.com", "password": "1234"},
+        )
+        self.assertEqual(response.status_code, 201)
