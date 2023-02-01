f59fe40cf5fae9098205106e8e5ade0a0425cd78
choding91 <whdlsrjf91@naver.com>
2023-02-01 21:36:28 +0900

    Test : 유저 로그인 테스트
    - 유저 로그인 테스트 케이스 작성 및 확인 완료
    Close #6

--- users/tests.py
+++ users/tests.py
@@ -4,9 +4,24 @@ from users.models import User
 
 
 class UserTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user("test@test.com", "1234")
+        cls.data = {"email": "test@test.com", "password": "1234"}
+
+    def setUp(self):
+        self.access_token = self.client.post(reverse("token_obtain_pair"), self.data).data["access"]
+
     def test_signup(self):
         response = self.client.post(
             path=reverse("signup_view"),
             data={"email": "email@email.com", "password": "1234"},
         )
         self.assertEqual(response.status_code, 201)
+
+    def test_login(self):
+        response = self.client.post(
+            path=reverse("token_obtain_pair"),
+            data=self.data,
+        )
+        self.assertEqual(response.status_code, 200)
