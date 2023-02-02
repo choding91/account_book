80ec217d966ef161b7a154c1e2fdfb9f7ad71983
choding91 <whdlsrjf91@naver.com>
2023-02-02 19:42:40 +0900

    Test : 가계부 전체 조회 테스트
    - 가계부 전체 조회 인증 여부 확인 완료
    Close #17

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from users.models import User
+from posts.models import Post
 
-# Create your tests here.
+
+class PostTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user("test@test.com", "1234")
+        cls.post = Post.objects.create(user=cls.user, account=10000, content="content")
+        cls.user_data = {"email": "test@test.com", "password": "1234"}
+        cls.post_data = {"account": 10000, "content": "content"}
+
+    def setUp(self):
+        self.access_token = self.client.post(reverse("token_obtain_pair"), self.user_data).data["access"]
+        self.refresh_token = self.client.post(reverse("token_obtain_pair"), self.user_data).data["refresh"]
+
+    def test_get_post(self):
+        response = self.client.get(
+            path=reverse("post_view"),
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
+        )
+        self.assertEqual(response.status_code, 200)
