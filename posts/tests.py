6a07be23cf9025e5744c71251aa50f3e5af9c41a
choding91 <whdlsrjf91@naver.com>
2023-02-02 19:49:49 +0900

    Test : 가계부 상세 조회 테스트
    - 가계부 상세 조회 및 인증 여부 확인 완료
    Close #19

--- posts/tests.py
+++ posts/tests.py
@@ -30,3 +30,23 @@ class PostTest(APITestCase):
             HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
         )
         self.assertEqual(response.status_code, 201)
+
+
+class PostDetailTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user("test@test.com", "1234")
+        cls.post = Post.objects.create(user=cls.user, account=10000, content="content")
+        cls.user_data = {"email": "test@test.com", "password": "1234"}
+        cls.post_data = {"account": 10000, "content": "content"}
+
+    def setUp(self):
+        self.access_token = self.client.post(reverse("token_obtain_pair"), self.user_data).data["access"]
+        self.refresh_token = self.client.post(reverse("token_obtain_pair"), self.user_data).data["refresh"]
+
+    def test_get_post(self):
+        response = self.client.get(
+            path=reverse("post_detail_view", kwargs={"post_id": self.post.id}),
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
+        )
+        self.assertEqual(response.status_code, 200)
